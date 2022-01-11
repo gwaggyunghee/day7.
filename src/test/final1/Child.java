a88de97fac3ddce04cfc7428b00bbb97e02364d9
82105 <82105@DESKTOP-N30VLLG>
2022-01-11 15:44:05 +0900

    .

---
+++ src/test/final1/Child.java
@@ -0,0 +1,12 @@
+package test.final1;
+
+public class Child extends Parent{
+	//final 필드는 나중에 값 변경 금지(final 필드 값 변경 금지)
+	public final int time=3;
+	
+	@Override
+	public void rest() {
+		//time = 5;
+		System.out.println(time+"시간 휴식한다.");
+	}
+}
